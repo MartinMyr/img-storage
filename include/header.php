f0d5ed5e2a684c8212423719c752d56ec30c1096
MartinMyr <32762130+MartinMyr@users.noreply.github.com>
2018-03-20 21:40:38 +0100

    index.php done with css

--- include/header.php
+++ include/header.php
@@ -6,6 +6,8 @@
     <title>Bildhanteraren</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
     <script src="main.js"></script>
 </head>
 <body>
--- index.php
+++ index.php
@@ -2,11 +2,17 @@
     include 'include/header.php';
     include 'include/function.php';
 ?>
-
-<form method = "post">
-    <input name = "user" type = "text">
-    <input name = "password" type = "password">
-    <button type = "submit">Logga in</button>
+<h1 class="headerText">BildBanken</h1>
+<form id="postBox" method="post">
+    <div class="inputwrapper">
+        <i class="fas fa-user"></i>
+        <input class="login" placeholder="Användarnamn" name="user" type="text">
+    </div>
+    <div class="inputwrapper">
+        <i class="fas fa-key"></i>
+        <input class="login" placeholder="Lösenord" name="password" type="password">
+    </div>
+    <button class = "login" type="submit">Logga in</button>
 </form>
 
 
